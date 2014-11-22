40552eb08bd7a853ce2cb9ce75a0e146493169ad
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2014-11-22 14:16:42 -0800

    added toggle to search form icon

---
+++ src/js/extras/hide-search.js
@@ -0,0 +1,11 @@
+/**
+ * Toggle header search on and off
+ */
+
+jQuery(document).ready(function($) {
+    $(".search-toggle").click(function(){
+        $("#search-container").slideToggle('slow', function(){
+            $(".search-toggle").addClass('active');
+        });
+    });
+});
